9e364ee019a45d0946b393522b4b642d429910be
Moussa75 <moussakone75011@gmail.com>
2024-01-09 01:53:45 +0100

    cmd terraform apply

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcp-terraform-project-410623-ae5f8d61f718.json")
+
+  project = "gcp-terraform-project-410623"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
